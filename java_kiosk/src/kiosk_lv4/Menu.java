0a1e61120cc350fb5cdfad542be2aef5ac6faec1
천세경 <jay45652@gmail.com>
2025-04-29 00:41:26 +0900

    [1-4] menuItems 입출력 form 수정

--- java_kiosk/src/kiosk_lv4/Menu.java
+++ java_kiosk/src/kiosk_lv4/Menu.java
@@ -28,14 +28,20 @@ public class Menu {
         menuItems.add(menuItem);
     }
 
+    public void addMenuItem(String name, int price, String description) {
+        menuItems.add(new MenuItem(name, price, description));
+    }
+
     public void addMenuItems(List<MenuItem> menuItems) {
         this.menuItems.addAll(menuItems);
     }
 
     public void printMenuItems() {
+        System.out.printf("[ %s MENU ]%n", category);
         for (int i = 0; i < menuItems.size(); i++) {
             MenuItem item = menuItems.get(i);
-            System.out.printf("%d. %-13s | W %.1f | %s\n", i + 1, item.getName(), (double) item.getPrice() / 1000, item.getDescription());
+            System.out.printf("%d. %-16s | W %.1f | %s\n", i + 1, item.getName(), (double) item.getPrice() / 1000, item.getDescription());
         }
+        System.out.println("0. 뒤로가기");
     }
 }
