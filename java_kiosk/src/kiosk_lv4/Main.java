d0fe6f122d96d40fd924885e37fb41c5ff34f535
천세경 <jay45652@gmail.com>
2025-04-28 23:25:21 +0900

    [1-4] Menu 카테고리 추가하는 로직

--- java_kiosk/src/kiosk_lv4/Main.java
+++ java_kiosk/src/kiosk_lv4/Main.java
@@ -2,8 +2,20 @@ package kiosk_lv4;
 
 import kiosk_lv3.Kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Menu burgers = new Menu("Burgers");
+        Menu Drinks = new Menu("Drinks");
+        Menu Desserts = new Menu("Desserts");
+
+        List<Menu> menuList = new ArrayList<>();
+        menuList.add(burgers);
+        menuList.add(Drinks);
+        menuList.add(Desserts);
+
         kiosk_lv3.Kiosk kiosk = new Kiosk();
         kiosk.start();
     }
