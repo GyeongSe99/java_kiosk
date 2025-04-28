65032f8aebef97744b761409a3d9fb65c7184154
천세경 <jay45652@gmail.com>
2025-04-28 22:03:28 +0900

    [1-4] kiosk_lv4 init

---
+++ java_kiosk/src/kiosk_lv4/Main.java
@@ -0,0 +1,10 @@
+package kiosk_lv4;
+
+import kiosk_lv3.Kiosk;
+
+public class Main {
+    public static void main(String[] args) {
+        kiosk_lv3.Kiosk kiosk = new Kiosk();
+        kiosk.start();
+    }
+}
---
+++ java_kiosk/src/kiosk_lv4/MenuItem.java
@@ -0,0 +1,37 @@
+package kiosk_lv4;
+
+public class MenuItem {
+    private String name;
+    private int price;
+    private String description;
+
+    public MenuItem(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
