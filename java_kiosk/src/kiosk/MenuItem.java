b436fa4cc279fd5eede081c13c66c565bb34f8ef
천세경 <jay45652@gmail.com>
2025-04-28 17:52:36 +0900

    [2-1] MenuItem 클래스 추가 이름, 가격, 설명 필드 생성. 각 필드의 생성자, getter, setter 추가

---
+++ java_kiosk/src/kiosk/MenuItem.java
@@ -0,0 +1,37 @@
+package kiosk;
+
+public class MenuItem {
+    private String name;
+    private String price;
+    private String description;
+
+    public MenuItem(String name, String price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
