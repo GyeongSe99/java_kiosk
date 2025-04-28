e0bedae93eac07e8fce612bad66a2727eb55a2dd
천세경 <jay45652@gmail.com>
2025-04-28 23:20:54 +0900

    [1-4] Menu 클래스에 필요한 함수 추가

--- java_kiosk/src/kiosk_lv4/Menu.java
+++ java_kiosk/src/kiosk_lv4/Menu.java
@@ -23,4 +23,19 @@ public class Menu {
     public void setCategory(String category) {
         this.category = category;
     }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public void addMenuItems(List<MenuItem> menuItems) {
+        this.menuItems.addAll(menuItems);
+    }
+
+    public void printMenuItems() {
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem item = menuItems.get(i);
+            System.out.printf("%d. %-13s | W %.1f | %s\n", i + 1, item.getName(), (double) item.getPrice() / 1000, item.getDescription());
+        }
+    }
 }
