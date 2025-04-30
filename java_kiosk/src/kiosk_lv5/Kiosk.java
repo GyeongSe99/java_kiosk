e91fef780980845bfd715e0654b6063361bae194
천세경 <jay45652@gmail.com>
2025-04-30 16:18:02 +0900

    [2-1] 2-1 init

---
+++ java_kiosk/src/kiosk_lv5/Kiosk.java
@@ -0,0 +1,75 @@
+package kiosk_lv5;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private final List<Menu> menus;
+    private Scanner sc = new Scanner(System.in);
+
+    public Kiosk() {
+        this.menus = new ArrayList<>();
+    }
+
+    public Kiosk(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public void start() {
+
+        while (true) {
+            printMenus();
+            int menuChoice = readIntInRange(0, this.menus.size());
+            if (menuChoice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                sc.close();
+                System.exit(0);
+            }
+            handleMenuItem(menus.get(menuChoice - 1));
+        }
+    }
+
+    private void printMenus() {
+        System.out.println("[ MAIN MENU ]");
+        for (int i = 0; i < this.menus.size(); i++) {
+            System.out.printf("%d. %s%n", i + 1, this.menus.get(i).getCategory());
+        }
+        System.out.println("0. 종료");
+    }
+
+    private void handleMenuItem(Menu menu) {
+        menu.printMenuItems();
+        List<MenuItem> menuItems = menu.getMenuItems();
+        int choiceMenuItem = readIntInRange(0, menuItems.size());
+        if (choiceMenuItem == 0) {
+            return;
+        }
+        MenuItem menuItem = menuItems.get(choiceMenuItem - 1);
+        System.out.printf("선택한 메뉴: %-16s | W %.1f | %s%n", menuItem.getName(), (double) menuItem.getPrice() / 1000, menuItem.getDescription());
+        System.out.println();
+    }
+
+    private int readIntInRange(int min, int max) {
+        int input;
+        while (true) {
+            try {
+                input = sc.nextInt();
+                if (input < min || input > max) {
+                    System.out.println("범위를 벗어났습니다. 다시 시도해주세요.");
+                    continue;
+                }
+                return input;
+            } catch (InputMismatchException e) {
+                System.out.println("정수만 입력해주세요.");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("시스템 오류가 발생했습니다.");
+                sc.nextLine();
+            }
+        }
+    }
+
+
+}
