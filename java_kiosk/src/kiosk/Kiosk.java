7df07bc7385d94a79f9fe040f90de47d588ea651
천세경 <jay45652@gmail.com>
2025-05-01 02:29:10 +0900

    [refactor] readIntInRange() 메소드 분리. Kiosk -> InputReader class로 위치변경

--- java_kiosk/src/kiosk/Kiosk.java
+++ java_kiosk/src/kiosk/Kiosk.java
@@ -11,13 +11,14 @@ import java.util.stream.Stream;
  */
 public class Kiosk {
     private final List<Menu> menus;
-    private Scanner sc = new Scanner(System.in);
-    private Cart cart;
+    private final InputReader inputReader;
+    private final Cart cart;
 
 
     public Kiosk(List<Menu> menus) {
         this.menus = menus;
         this.cart = new Cart();
+        this.inputReader = new InputReader();
     }
 
     /**
@@ -35,7 +36,7 @@ public class Kiosk {
             // 유효 범위 계산 (0: 종료, 1~N: 메뉴, N+1~N+2: 주문/취소)
             int choiceMaxRange = menus.size() + (canOrder ? 2 : 0);
             // 사용자 입력 받기
-            int menuChoice = readIntInRange(0, choiceMaxRange);
+            int menuChoice = inputReader.readIntInRange(0, choiceMaxRange);
             // 입력값에 따른 분기 처리
             processChoice(menuChoice, canOrder);
         }
@@ -64,7 +65,6 @@ public class Kiosk {
 
     private void handleExit() {
         System.out.println("프로그램을 종료합니다.");
-        sc.close();
         System.exit(0);
     }
 
@@ -78,12 +78,12 @@ public class Kiosk {
         int totalPrice = cart.showTotalPrice();
         System.out.println("1. 주문        2. 메뉴판");
 
-        int selectedNum = readIntInRange(1, 2);
+        int selectedNum = inputReader.readIntInRange(1, 2);
         if (selectedNum == 1) {
             System.out.println("할인 정보를 입력해주세요.");
             Stream.of(UserType.values())
                     .forEach(type -> System.out.printf("%d. %s : %d%%%n", type.ordinal()+1, type.getLabel(), type.getRate()));
-            int selectedNumForUserType = readIntInRange(1, UserType.values().length);
+            int selectedNumForUserType = inputReader.readIntInRange(1, UserType.values().length);
             UserType userType = UserType.values()[selectedNumForUserType - 1];
             double discounted = totalPrice * (1 - userType.getRate() / 100.0);
             System.out.printf("주문이 완료되었습니다. 금액은 W %.1f 입니다.%n", (double) discounted / 1000);
@@ -105,7 +105,7 @@ public class Kiosk {
     private void handleMenuItem(Menu menu) {
         menu.showMenuItems();
         List<MenuItem> menuItems = menu.getMenuItems();
-        int choiceMenuItem = readIntInRange(0, menuItems.size());
+        int choiceMenuItem = inputReader.readIntInRange(0, menuItems.size());
         if (choiceMenuItem == 0) {
             return;
         }
@@ -116,25 +116,7 @@ public class Kiosk {
         System.out.println();
     }
 
-    private int readIntInRange(int min, int max) {
-        int input;
-        while (true) {
-            try {
-                input = sc.nextInt();
-                if (input < min || input > max) {
-                    System.out.println("범위를 벗어났습니다. 다시 시도해주세요.");
-                    continue;
-                }
-                return input;
-            } catch (InputMismatchException e) {
-                System.out.println("정수만 입력해주세요.");
-                sc.nextLine();
-            } catch (Exception e) {
-                System.out.println("시스템 오류가 발생했습니다.");
-                sc.nextLine();
-            }
-        }
-    }
+
 
     /**
      * 장바구니에 아이템을 추가할지 사용자에게 확인
@@ -142,7 +124,7 @@ public class Kiosk {
     private void handleAddToCart(MenuItem menuItem) {
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.println("1. 확인        2. 취소");
-        int selectedNum = readIntInRange(1, 2);
+        int selectedNum = inputReader.readIntInRange(1, 2);
         if (selectedNum == 1) {
             cart.addToCart(menuItem);
             System.out.printf("%s이 장바구니에 추가되었습니다. %n", menuItem.getName());
