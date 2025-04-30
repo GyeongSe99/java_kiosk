0ff3f825568bd3af0ffabf28962551be2cff2361
천세경 <jay45652@gmail.com>
2025-05-01 00:45:28 +0900

    [2-3] UserType Enum 추가

---
+++ java_kiosk/src/kiosk_lv6/UserType.java
@@ -0,0 +1,24 @@
+package kiosk_lv6;
+
+public enum UserType {
+    NATIONAL_HONOREE("국가유공자", 10),
+    SOLDIER("군인", 5),
+    STUDENT("학생", 3),
+    GENERAL("일반", 0);
+
+    private final String label;
+    private final int rate;
+
+    UserType(String label, int rate) {
+        this.label = label;
+        this.rate = rate;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+}
